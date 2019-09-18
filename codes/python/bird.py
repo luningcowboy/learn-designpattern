202f4e8d3175ebe28a27413a049cabba530d050d
luningcowboy <luningcowboy@gmail.com>
2019-09-18 16:01:22 +0800

    add code

---
+++ codes/python/bird.py
@@ -0,0 +1,8 @@
+class Bird(object):
+    def __init__(self):
+        self.speed = 0
+        self.flyTime = 0
+    def setSpeed(self, speed):
+        self.speed = speed
+    def getFlyTime(self):
+        return self.flyTime
