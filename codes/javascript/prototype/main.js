a46ed7c69ac26f0d1392d363130c0d65c2ba9cc5
luningcowboy <luningcowboy@gmail.com>
2019-11-12 22:20:39 +0800

    add prototype

--- codes/javascript/prototype/main.js
+++ codes/javascript/prototype/main.js
@@ -1,7 +1,7 @@
 class Shape{
-    _id = ''
-    _type = ''
     draw(){
+        this._id = '';
+        this._type = '';
     }
     getType(){
         return this._type;
@@ -21,6 +21,7 @@ class Shape{
 }
 class Rectagle extends Shape{
     constructor(){
+        super();
         this._type = 'Rectagle';
     }
     clone(){
@@ -33,3 +34,69 @@ class Rectagle extends Shape{
         console.log('Draw Rectagle');
     }
 }
+class Square extends Shape{
+    constructor(){
+        super();
+        this._type = 'square';
+    }
+    draw(){
+        super.draw();
+        console.log('Draw square');
+    }
+    clone(){
+        return new Square();
+    }
+}
+class Circle extends Shape{
+    constructor(){
+        super();
+        this._type = 'circle';
+    }
+    draw(){
+        super.draw();
+        console.log("Draw circle");
+    }
+    clone(){
+        super.clone();
+        return new Circle();
+    }
+}
+let _instance = null;
+class ShapeCache{
+    static getInstance(){
+        if(!_instance){
+            _instance = new ShapeCache();
+        }
+        return _instance;
+    }
+    constructor(){
+        this._shapeMap = new Map();
+    }
+    loadCache(){
+        let circle = new Circle();
+        circle.setId("1");
+        this._shapeMap.set(circle.getId(), circle);
+
+        let square = new Square();
+        square.setId("2");
+        this._shapeMap.set(square.getId(), square);
+
+        let rectangle = new Rectagle();
+        rectangle.setId("3");
+        this._shapeMap.set(rectangle.getId(), rectangle);
+    }
+    getShape(id){
+        return this._shapeMap.get(id).clone();
+    }
+}
+
+function main(){
+    ShapeCache.getInstance().loadCache();
+    let cloneShape = ShapeCache.getInstance().getShape("1");
+    let cloneShape1 = ShapeCache.getInstance().getShape("2");
+    let cloneShape2 = ShapeCache.getInstance().getShape("3");
+    console.log(cloneShape.getType());
+    console.log(cloneShape1.getType());
+    console.log(cloneShape2.getType());
+}
+main();
