6049ff5a2b29077cc1165775c9d50e7c263b2568
luningcowboy <luningcowboy@gmail.com>
2019-12-30 16:15:16 +0800

    new code

--- codes/javascript/prototype/test.js
+++ codes/javascript/prototype/test.js
@@ -1,7 +1,16 @@
 (function() {
-    function Person() {}
-    let TestObj = {};
+    function Person() {
+        this._a = 0;
+    }
+    let TestObj = {
+        "a": 1
+    };
     // 只有函数才有prototype
     console.log(Person.prototype);//Person {}
     console.log(TestObj.prototype);//undefined
+    // getOwnPropertyNames 并不是获取prototype
+    console.log(Object.getOwnPropertyNames(Person));//[ 'length', 'name', 'arguments', 'caller', 'prototype' ]
+    console.log(Object.getOwnPropertyNames(TestObj));//[ 'a' ]
+    let p = new Person();
+    console.log(Object.getOwnPropertyNames(p));// [ '_a']
 })();
